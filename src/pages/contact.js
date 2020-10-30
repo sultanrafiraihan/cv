61bcc87301ce4e251ade875c712f98b7331c1298
sultanrafiraihan <sultanrafi21@gmail.com>
2020-10-30 13:15:46 +0700

    cv hooks update

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,25 +12,29 @@ export function Contact() {
         <title>Contact</title>
       </Helmet>
       <div className="Kontak">
-        <div className="col-lg">
-          <p>alamat : </p>
-          <p>
-            Perumahan Permata Regensi Blok I2/12A, Desa Wanasari, Kecamatan
-            Cibitung, Kabupaten Bekasi – 17520
-          </p>
+        <div className="container">
+          <div className="col-lg">
+            <div>
+              <h3>Pengguna</h3>
+              <div>{value}</div>
+            </div>
+          </div>
+          <div className="col-lg">
+            <p>alamat : </p>
+            <p>
+              Perumahan Permata Regensi Blok I2/12A, Desa Wanasari, Kecamatan
+              Cibitung, Kabupaten Bekasi – 17520
+            </p>
+          </div>
+          <div className="col-lg">
+            <p>telepon :</p>
+            <p>085972930234 / 081292988158</p>
+          </div>
+          <div className="col-lg">
+            <p>email :</p>
+            <p>sultanrafi21@gmail.com</p>
+          </div>
         </div>
-        <div className="col-lg">
-          <p>telepon :</p>
-          <p>085972930234 / 081292988158</p>
-        </div>
-        <div className="col-lg">
-          <p>email :</p>
-          <p>sultanrafi21@gmail.com</p>
-        </div>
-      </div>
-      <div>
-        <h2>Home</h2>
-        <div>{value}</div>
       </div>
     </HelmetProvider>
   );
